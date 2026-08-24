871ee2b53b89f996c1fbdcfd05fbbd1545c2ecbe
Thiago Alonso <163365934+Thiagoalfs@users.noreply.github.com>
2026-08-24 20:41:57 -0300

    path fix

--- api/servidores_api.php
+++ api/servidores_api.php
@@ -10,7 +10,7 @@
  * (aqui assumimos que config.php está uma pasta acima de /api/).
  */
 
-require_once "../config.php";
+require_once "../../config.php";
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
